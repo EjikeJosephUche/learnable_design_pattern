6b8c7672106693789800d43d6885b387fb064ac6
EjikeJosephUche <uche.ejike.123@gmail.com>
2025-02-15 12:34:26 -0800

    Added some comments to the code

--- app.js
+++ app.js
@@ -3,23 +3,27 @@
  * Description: An assignment to test my knowledge on various design patters
  */
 
+// The Telephone class maintains the list of observers and telephoneNumbers
 class Telephone {
     constructor(){
         this.telephoneNumbers = new Set()
         this.observers = []
     }
 
+    // adds a phone number to the list
     addPhoneNumber(phoneNumber){
         this.telephoneNumbers.add(phoneNumber)
         console.log(`${phoneNumber} has been added!!!`)
     }
     
+    // removes a phone number from the list
     removePhoneNumber(phoneNumber){
         this.telephoneNumbers.delete(phoneNumber)
         console.log(`${phoneNumber} has been removed!!!`)
         
     }
 
+    // dials a phone number already added to the list and notifies the observers in the list
     dialPhoneNumber(phoneNumber){
         const findPhoneNumber = this.telephoneNumbers.has(phoneNumber)
         if (findPhoneNumber){
@@ -30,15 +34,18 @@ class Telephone {
         }
     }
 
+    // adds an observer to the list of observer
     addObserver(observer){
         this.observers.push(observer)
         console.log(`${observer.name} has been added`)
     }
 
+    // removes an observer from the list of observer
     removeObserver(observer){
         this.observers = this.observers.filter(obs => obs.name !== observer.name)
     }
 
+    // loops through the list of observers and notifies them of any changes.
     notifyObservers(Message){
         this.observers.forEach(observer => {
             observer.notify(Message)
@@ -47,6 +54,7 @@ class Telephone {
     }
 }
 
+// The observer class to notify the list of observers
 class Observers {
     constructor(name){
         this.name = name
@@ -56,6 +64,7 @@ class Observers {
     }
 }
 
+// Tests 
 const telephone = new Telephone();
 const joseph = new Observers("Joseph")
 const ejike = new Observers("Ejike")
